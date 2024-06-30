682c6d81e1c89d6fdbe41d5bd75435cd93e4c30a
mahiedine.ferdjoukh.etu <mahiedine.ferdjoukh.etu@univ-lille.fr>
2024-06-30 15:13:27 +0200

    pokemon model

---
+++ pokemonapp/src/app/models/pokemon.ts
@@ -0,0 +1,21 @@
+
+
+export class Pokemon{
+
+
+    constructor(
+        public name : string, 
+        public sprites: {
+            front_default: string;
+          }, 
+        public types : { slot: number; type: { name: string; }; }[], 
+        public height : number, 
+        public weight : number, 
+        public id : number
+    ){} 
+
+
+
+
+
+} 
