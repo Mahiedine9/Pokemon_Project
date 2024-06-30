85395c00f4a58fe917f927795cbe6a8079cd2f69
mahiedine.ferdjoukh.etu <mahiedine.ferdjoukh.etu@univ-lille.fr>
2024-06-30 15:28:16 +0200

    add search bar

---
+++ pokemonapp/src/app/pokemon-search-bar/pokemon-search-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-pokemon-search-bar',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './pokemon-search-bar.component.html',
+  styleUrl: './pokemon-search-bar.component.scss'
+})
+export class PokemonSearchBarComponent {
+  searchName: string = '';
+
+  @Output() search: EventEmitter<string> = new EventEmitter<string>();
+
+  onSearch() {
+    this.search.emit(this.searchName);
+  }
+
+
+
+}
