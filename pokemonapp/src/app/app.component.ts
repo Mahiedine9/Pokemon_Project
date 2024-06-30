4bcbc7b8cf59484cab0447586bfe4a60f1c728ab
mahiedine.ferdjoukh.etu <mahiedine.ferdjoukh.etu@univ-lille.fr>
2024-06-30 23:13:04 +0200

    remove a search bar from global app

--- pokemonapp/src/app/app.component.ts
+++ pokemonapp/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { PokemonComponent } from './pokemon/pokemon.component';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PokemonComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pokemonapp';
+
+  constructor(private router : Router) {}
+
+
 }
