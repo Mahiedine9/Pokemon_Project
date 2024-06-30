dd67cd86d9d6c49b96b67fa4d6916b7ae6022040
mahiedine.ferdjoukh.etu <mahiedine.ferdjoukh.etu@univ-lille.fr>
2024-06-30 23:13:45 +0200

    add paths

--- pokemonapp/src/app/app.routes.ts
+++ pokemonapp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'pokemons/:name', component: PokemonComponent },
+    { path: '**', redirectTo: '' }
+];
