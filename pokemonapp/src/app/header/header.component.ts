ee37dd72ab62e1fbbfa884f20f59d72bdacbfe83
mahiedine.ferdjoukh.etu <mahiedine.ferdjoukh.etu@univ-lille.fr>
2024-07-02 21:27:54 +0200

    update header

--- pokemonapp/src/app/header/header.component.ts
+++ pokemonapp/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { PokemonSearchBarComponent } from '../pokemon-search-bar/pokemon-search-bar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, PokemonSearchBarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
