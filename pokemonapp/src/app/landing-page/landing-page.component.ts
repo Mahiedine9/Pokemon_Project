24fa3db67f22a027df0bd1bdc234ab5649f8480c
mahiedine.ferdjoukh.etu <mahiedine.ferdjoukh.etu@univ-lille.fr>
2024-07-02 23:44:27 +0200

    update landing page

--- pokemonapp/src/app/landing-page/landing-page.component.ts
+++ pokemonapp/src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonSearchBarComponent } from '../pokemon-search-bar/pokemon-search-bar.component';
+import { PokemonComponent } from '../pokemon/pokemon.component';
+import { PokemonsService } from '../services/pokemons.service';
+import { Pokemon } from '../models/pokemon';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [ ],
+  imports: [CommonModule, HttpClientModule, PokemonSearchBarComponent, PokemonComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  pokemonTypes = [
+    { name: 'fire' }, { name: 'water' }, { name: 'grass' }, { name: 'electric' },
+  ];
+  featuredPokemons: Pokemon[] = [];
+  recentSearches: Pokemon[] = [];
 
+  constructor(private pokemonsService: PokemonsService, private router: Router) {}
 
+  ngOnInit(): void {
+    this.loadFeaturedPokemons();
+    this.loadRecentSearches();
+  }
 
+  loadFeaturedPokemons(): void {
+    const featuredNames = ['pikachu', 'charizard', 'bulbasaur'];
+    const requests = featuredNames.map(name => this.pokemonsService.fetchPokemonByName(name));
+    Promise.all(requests).then(pokemons => this.featuredPokemons = pokemons);
+  }
+
+  loadRecentSearches(): void {
+    const recentSearches = localStorage.getItem('recentSearches');
+    if (recentSearches) {
+      this.recentSearches = JSON.parse(recentSearches);
+    }
+  }
+
+  navigateToType(type: { name: string }): void {
+    this.router.navigate(['/type', type.name]);
+  }
 }
