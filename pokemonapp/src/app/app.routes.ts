ac273b8295789c7ad25a2a9d736243b8002d373e
Mahiedine9 <mahieddine.ferdjoukh9@gmail.com>
2024-07-10 22:23:41 +0200

    Hire me component

--- pokemonapp/src/app/app.routes.ts
+++ pokemonapp/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'pokemons/:name', component: PokemonComponent },
     { path: 'type/:type', component: PokemonListComponent },
+    { path : 'contacts', component : ContactComponent},
     { path: '**', redirectTo: '' }
 ];
